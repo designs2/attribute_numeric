516be145eaf5cd0cf39832bec7d61e2481f116d8
Stefan Heimes <stefan_heimes@hotmail.com>
2013-06-27 23:59:06 +0200

    Implements the methods filterGreaterThan() filterLessThan() filterNotEqual().

--- src/system/modules/metamodelsattribute_numeric/MetaModelAttributeNumeric.php
+++ src/system/modules/metamodelsattribute_numeric/MetaModelAttributeNumeric.php
@@ -48,4 +48,71 @@ class MetaModelAttributeNumeric extends MetaModelAttributeSimple
 		$arrFieldDef['eval']['rgxp'] = 'digit';
 		return $arrFieldDef;
 	}
+
+	////////////////////////////////////////////////////////////////////////////
+	// IMetaModelAttribute functions
+	////////////////////////////////////////////////////////////////////////////
+
+	/**
+	 * Filter all values greater than the passed value.
+	 *
+	 * @param mixed $varValue     The value to use as lower end
+	 *
+	 * @param bool  $blnInclusive If true, the passed value will be included, if false, it will be excluded.
+	 *
+	 * @return int[] The list of item ids of all items matching the condition.
+	 */
+	public function filterGreaterThan($varValue, $blnInclusive = false)
+	{
+		return $this->getIdsFiltered($varValue, ($blnInclusive) ? '>=' : '>');
+	}
+
+	/**
+	 * Filter all values less than the passed value.
+	 *
+	 * @param mixed $varValue     The value to use as upper end.
+	 *
+	 * @param bool  $blnInclusive If true, the passed value will be included, if false, it will be excluded.
+	 *
+	 * @return int[] The list of item ids of all items matching the condition.
+	 */
+	public function filterLessThan($varValue, $blnInclusive = false)
+	{
+		return $this->getIdsFiltered($varValue, ($blnInclusive) ? '<=' : '<');
+	}
+
+	/**
+	 * Filter all values not having the passed value.
+	 *
+	 * @param mixed $varValue     The value to use as upper end.
+	 *
+	 * @return array The list of item ids of all items matching the condition.
+	 */
+	public function filterNotEqual($varValue)
+	{
+		return $this->getIdsFiltered($varValue, '!=');
+	}
+
+	/**
+	 * Filter all values by specified operation.
+	 *
+	 * @param int    $varValue     The value to use as upper end.
+	 *
+	 * @param string $strOperation The specified operation like greater than, lower than etc.
+	 *
+	 * @return int[] The list of item ids of all items matching the condition.
+	 */
+	protected function getIdsFiltered($varValue, $strOperation)
+	{
+		$strSql = sprintf('SELECT id FROM %s WHERE %s %s %d',
+				$this->getMetaModel()->getTableName(),
+				$this->getColName(),
+				$strOperation,
+				intval($varValue)
+				);
+
+		$objIds = Database::getInstance()->executeUncached($strSql);
+		return $objIds->fetchEach('id');
+	}
+
 }
